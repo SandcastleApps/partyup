3fca0c52128e7650713ae3514928806ce0372579
Fritz Vander Heide <etherboy@me.com>
2015-09-19 14:44:38 -0300

    Added the handy UrlUtilities source file to the project to make use of the extension providing the URLs mime type.

---
+++ PartyUP/UrlUtilities.swift
@@ -0,0 +1,28 @@
+//
+//  UrlUtilities.swift
+//  MediaMonkey
+//
+//  Created by Fritz Vander Heide on 2015-09-07.
+//  Copyright © 2015 AppleTrek. All rights reserved.
+//
+
+import Foundation
+import MobileCoreServices
+
+extension NSURL
+{
+	var mime: String {
+		get{
+			var mime: String = "application/octet-stream"
+			if let ext = self.pathExtension {
+				if let cfUti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, ext, nil) {
+					if let cfMime = UTTypeCopyPreferredTagWithClass(cfUti.takeRetainedValue() as String, kUTTagClassMIMEType) {
+						mime = cfMime.takeRetainedValue() as String
+					}
+				}
+			}
+			
+			return mime
+		}
+	}
+}
