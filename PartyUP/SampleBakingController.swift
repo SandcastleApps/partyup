3d48ab28b1ab27fc35bdcb5addcdbd8e4b9e1eaf
Fritz Vander Heide <etherboy@me.com>
2015-10-01 22:18:52 -0300

    Added a timer and timer progress bar that will restrict the user to recording for 10 seconds.

--- PartyUP/SampleBakingController.swift
+++ PartyUP/SampleBakingController.swift
@@ -11,18 +11,19 @@ import UIKit
 class SampleBakingController: UIViewController, VideoRecorderDelegate {
 
 	@IBOutlet weak var recordButton: UIButton!
+	@IBOutlet weak var timerBar: UIProgressView!
+
+	var timer: NSTimer!
 
 	var recordingController: VideoRecordController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+		timerBar.transform = CGAffineTransformScale(timerBar.transform, 1.5, 1.75)
+        timerBar.transform = CGAffineTransformRotate(timerBar.transform, CGFloat(3*M_PI/2))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+		timer = NSTimer(timeInterval: 1.0, target: self, selector: Selector("observeTimerInterval"), userInfo: nil, repeats: true)
     }
 
 	override func prefersStatusBarHidden() -> Bool {
@@ -42,12 +43,25 @@ class SampleBakingController: UIViewController, VideoRecorderDelegate {
 
 	// MARK: - Recording
 
-	@IBAction func startRecording(sender: UIButton) {
-		recordingController.start()
+	func observeTimerInterval() {
+		timerBar.progress += 0.1
+		if timerBar.progress >= 0.5 {
+			timerBar.progressTintColor = UIColor.greenColor()
+		}
+
+		if timerBar.progress >= 1.0 {
+			stopRecording()
+		}
 	}
 
-	@IBAction func stopRecording(sender: UIButton) {
+	@IBAction func startRecording() {
+		recordingController.start()
+		NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+	}
+
+	@IBAction func stopRecording() {
 		recordingController.stop()
+		timer.invalidate()
 	}
 
 	// MARK: - Video Recorder Delegate
