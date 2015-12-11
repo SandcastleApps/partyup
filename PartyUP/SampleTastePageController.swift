0a0da51b75566f8a9bd7818983ea3195447f027c
Fritz Vander Heide <etherboy@me.com>
2015-12-11 12:30:51 -0400

    Fixed a stuttering issue when the video was first loaded, started playing but did not have enough cached to continue and the video got restarted.

--- PartyUP/SampleTastePageController.swift
+++ PartyUP/SampleTastePageController.swift
@@ -136,7 +136,9 @@ class SampleTastePageController: UIViewController, PageProtocol, PlayerDelegate 
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
 		player.delegate = self
-		player.playFromBeginning()
+		if player.bufferingState == .Ready {
+			player.playFromBeginning()
+		}
 
 		Flurry.logEvent("Sample_Tasted", withParameters: ["timestamp" : sample.time.description], timed: true)
 	}
@@ -171,6 +173,9 @@ class SampleTastePageController: UIViewController, PageProtocol, PlayerDelegate 
 
 	func playerReady(player: Player) {
 		videoWaiting.stopAnimating()
+		if player.playbackState != .Playing {
+			player.playFromBeginning()
+		}
 	}
 
 	func playerPlaybackStateDidChange(player: Player) {
@@ -187,6 +192,9 @@ class SampleTastePageController: UIViewController, PageProtocol, PlayerDelegate 
 	}
 
 	func playerBufferingStateDidChange(player: Player) {
+		if player.bufferingState == .Ready {
+			//player.playFromBeginning()
+		}
 	}
 
 	// MARK: Timer
