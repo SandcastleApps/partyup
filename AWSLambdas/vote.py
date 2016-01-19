e81cf35231e77d64f619169fc0625c0ae7d0edc8
Fritz Vander Heide <etherboy@me.com>
2016-01-19 18:00:50 -0400

    Determine rating dispositions noted by the data changes and store them in a dictionary with the sample identification as the key.

--- AWSLambdas/vote.py
+++ AWSLambdas/vote.py
@@ -1,6 +1,6 @@
 """
     Watch Votes stream and update Sample ups and downs
-    """
+"""
 
 import json
 import boto3
@@ -11,7 +11,18 @@ from boto3.dynamodb.conditions import Key, Attr
 def vote_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Samples')
+    
+    ratings = dict()
 
     for record in event['Records']:
-        print(record['dynamodb']['NewImage'])
+        type = record['eventName']
+        disposition = 0
+        if type == "INSERT" or type == "MODIFY":
+            disposition = int(record['dynamodb']['NewImage']['vote']['N'])
+        if type == "MODIFY" or type == "REMOVE":
+            disposition += -int(record['dynamodb']['OldImage']['vote']['N'])
+        sample = record['dynamodb']['Keys']['sample']['B']
+        ratings[sample] = ratings.get(sample, 0) + disposition
+
+
 
