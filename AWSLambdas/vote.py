450dcfaa2e0c6c9fc7a351616f5722af26124040
Fritz Vander Heide <etherboy@me.com>
2016-01-20 12:42:47 -0400

    Split the sample identification into its id and event parts so that they can be used as a hash and range key in the Samples table.

--- AWSLambdas/vote.py
+++ AWSLambdas/vote.py
@@ -6,15 +6,11 @@ import json
 import boto3
 import time
 import decimal
+import base64
 from boto3.dynamodb.conditions import Key, Attr
 
-def vote_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Samples')
-    
-    ratings = dict()
-
-    for record in event['Records']:
+def consolidate_disposition(disposition_map, records):
+    for record in records:
         type = record['eventName']
         disposition = 0
         if type == "INSERT" or type == "MODIFY":
@@ -22,7 +18,19 @@ def vote_handler(event, context):
         if type == "MODIFY" or type == "REMOVE":
             disposition += -int(record['dynamodb']['OldImage']['vote']['N'])
         sample = record['dynamodb']['Keys']['sample']['B']
-        ratings[sample] = ratings.get(sample, 0) + disposition
+        disposition_map[sample] = disposition_map.get(sample, 0) + disposition
 
 
+def vote_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Samples')
+    
+    ratings = dict()
+    consolidate_disposition(ratings, event['Records'])
 
+    for (sample, vote) in ratings.iteritems():
+        ident = sample[0:19]
+        event = base64.standard_b64decode(sample[18:])
+        print ident
+        print event
+    
