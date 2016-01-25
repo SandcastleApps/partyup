207dd25e22a7926a6203a82e3636bff821f17805
Fritz Vander Heide <etherboy@me.com>
2016-01-25 11:44:38 -0400

    Purge the votes related to the samples that were already being purged.

--- AWSLambdas/purge.py
+++ AWSLambdas/purge.py
@@ -5,33 +5,49 @@ import logging
 import json
 import boto3
 import time
-import decimal
+from decimal import Decimal
+from boto3.dynamodb.types import Binary
 from boto3.dynamodb.conditions import Key, Attr
 
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
 
-def purge_item(item, batch):
-    response = batch.delete_item(
-        Key={
-            'event' : item['event'],
-            'id': item['id']
-        }
-    )
+def purge_vote(vote, votes_batch):
+    votes_batch.delete_item(Key={'sample' : vote['sample'], 'user': vote['user']})
+
+def purge_sample(sample, vote_table, samples_batch, votes_batch):
+    samples_batch.delete_item(Key={'event' : sample['event'], 'id': sample['id']})
+    sample_identity =  Binary(sample['id'].value + bytearray(sample['event'], "utf_8"))
+    sample_key = Key('sample').eq(sample_identity)
+
+    response = vote_table.query(KeyConditionExpression=sample_key)
+
+    for item in response['Items']:
+        purge_vote(item, votes_batch)
+
+        while 'LastEvaluatedKey' in response:
+            response = query(KeyConditionExpression=sample_key, ExclusiveStartKey=response['LastEvaluatedKey'])
+
+            for item in response['Items']:
+                purge_vote(item, votes_batch)
+
+
 
 def purge_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
 
-    table = dynamodb.Table('Samples')
-    filter = Attr('time').lte(decimal.Decimal(time.time()-172800))
+    sample_table = dynamodb.Table('Samples')
+    vote_table = dynamodb.Table('Votes')
 
-    with table.batch_writer() as batch:
-        response = table.scan(
+    filter = Attr('time').lte(Decimal(time.time()-172800))
+
+    with sample_table.batch_writer() as samples_batch, vote_table.batch_writer() as votes_batch:
+        response = sample_table.scan(
             FilterExpression=filter
         )
 
         for item in response['Items']:
-            purge_item(item, batch)
+            purge_sample(item, vote_table, samples_batch, votes_batch)
 
         while 'LastEvaluatedKey' in response:
             response = scan(
@@ -40,4 +56,4 @@ def purge_handler(event, context):
             )
 
             for item in response['Items']:
-                purge_item(item, batch)
+                purge_sample(item, vote_table, samples_batch, votes_batch)
