aac025a0f7d4c61a34531b1b8018598ae895d84f
Fritz Vander Heide <etherboy@me.com>
2016-02-27 22:13:08 -0400

    Break out the share composition controller creation from the share activity sheet so that it can be called from anywhere.

--- PartyUP/ShareUtilities.swift
+++ PartyUP/ShareUtilities.swift
@@ -10,6 +10,27 @@ import UIKit
 import Social
 import Flurry_iOS_SDK
 
+func presentShareSheetOn(presenting: UIViewController,
+	viaService service: String,
+	withMessage message: String,
+	url: NSURL?,
+	image: UIImage?) {
+	if SLComposeViewController.isAvailableForServiceType(service) {
+		let shareSheet:SLComposeViewController = SLComposeViewController(forServiceType: service)
+		shareSheet.setInitialText(message)
+		shareSheet.addURL(url)
+		shareSheet.addImage(image)
+		presenting.presentViewController(shareSheet, animated: true, completion:  nil)
+	} else {
+		let alert = UIAlertController(
+			title: NSLocalizedString("Accounts", comment: "Share login alert title"),
+			message: NSLocalizedString("Please login to a \(service.componentsSeparatedByString(".").last!) account to share.", comment: "Share login alert message"), preferredStyle: UIAlertControllerStyle.Alert)
+		alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Share login alert action"), style: .Default, handler: nil))
+		presenting.presentViewController(alert, animated: true, completion: nil)
+	}
+}
+
+
 func presentShareActionsOn(presenting: UIViewController,
 		atOrigin origin: UIView,
         withPrompt prompt: String,
@@ -20,24 +41,10 @@ func presentShareActionsOn(presenting: UIViewController,
 	let services = ["Facebook" : SLServiceTypeFacebook, "Twitter" : SLServiceTypeTwitter]
 
 	for (title, service) in services {
-		sheet.addAction(UIAlertAction(title: title, style: .Default, handler:
-			{ (action) in
-				if SLComposeViewController.isAvailableForServiceType(service){
-					let shareSheet:SLComposeViewController = SLComposeViewController(forServiceType: service)
-					shareSheet.setInitialText(message)
-					shareSheet.addURL(url)
-					shareSheet.addImage(image)
-					presenting.presentViewController(shareSheet, animated: true) {
-						Flurry.logEvent(prompt, withParameters: ["service" : title])
-					}
-				} else {
-					let alert = UIAlertController(
-						title: NSLocalizedString("Accounts", comment: "Share login alert title"),
-						message: NSLocalizedString("Please login to a \(title) account to share.", comment: "Share login alert message"), preferredStyle: UIAlertControllerStyle.Alert)
-					alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Share login alert action"), style: UIAlertActionStyle.Default, handler: nil))
-					presenting.presentViewController(alert, animated: true, completion: nil)
-				}
-		}))
+		sheet.addAction(UIAlertAction(title: title, style: .Default) { _ in
+			presentShareSheetOn(presenting, viaService: service, withMessage: message, url: url, image: image)
+			Flurry.logEvent(prompt, withParameters: ["service" : title])
+		})
 	}
 	sheet.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Share sheet cancel action"), style: .Cancel, handler: nil))
     
