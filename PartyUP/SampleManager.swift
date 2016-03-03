7d32d53103a0054496221b9d2c8f66a7cb453d1a
Fritz Vander Heide <etherboy@me.com>
2016-03-03 18:21:08 -0400

    Remove SampleManager and implement part of SampleSubmission.  This early version does not build.

--- PartyUP/SampleManager.swift
+++ PartyUP/SampleManager.swift
@@ -11,93 +11,71 @@ import AWSS3
 import AWSDynamoDB
 import AWSCore
 
-
-class SampleManager
+class SampleSubmission
 {
-	enum SampleManagerError: ErrorType
-	{
-		case TransferUtilityUnavailable
-		case InvalidFileName(url: NSURL)
-		case SubmissionError(error: NSError)
-		case SubmissionException(exception: NSException)
-	}
+    let sample: Sample
+    lazy var name: String? = { return self.sample.media.path.flatMap({String($0.characters.dropFirst())}) }()
+    lazy var file: NSURL? = { return self.name.flatMap({NSURL(fileURLWithPath: NSTemporaryDirectory() + $0)}) }()
+    
+    init(sample: Sample) throws {
+        self.sample = sample
+    }
+    
+    func process() throws {
+        switch state {
+        case .Idle:
+            try upload()
+        case .Upload(let task):
+            if task.faulted {
+                try upload()
+            } else {
+                try record()
+            }
+        case .Record(let task):
+            if task.faulted {
+                try record()
+            }
+        }
+    }
+    
+    private func upload() throws {
+        guard let transfer = AWSS3TransferUtility.defaultS3TransferUtility() else { throw SubmissionError.TransferUtilityUnavailable }
+        guard let videoUrl = file else { throw SubmissionError.InvalidFileName(url: sample.media) }
+        guard let videoName = name else { throw SubmissionError.InvalidFileName(url: sample.media) }
+        
+        let uploadExpr = AWSS3TransferUtilityUploadExpression()
+        uploadExpr.setValue("REDUCED_REDUNDANCY", forRequestParameter: "x-amz-storage-class")
+        
+        let task = transfer.uploadFile(videoUrl,
+            bucket: PartyUpConstants.StorageBucket,
+            key: videoName,
+            contentType: videoUrl.mime,
+            expression: uploadExpr,
+            completionHander: nil).continueWithBlock { _ in self.process() }
+        state = .Upload(task: task)
+    }
+    
+    private func record() throws {
+        state = AWSDynamoDBObjectMapper.defaultDynamoDBObjectMapper().save(sample.dynamo)})
+        state.continueWithBlock({ (task) in
+            dispatch_async(dispatch_get_main_queue()) { self.process(task) }
+            return nil
+        })
+    }
 
-	typealias SampleSubmission = (sample: Sample, completion: (ErrorType?)->Void)
-	private var queue = [(SampleSubmission)]()
-	private var active: SampleSubmission?
-
-	private static let sharedManager = SampleManager()
-
-	static func defaultManager() -> SampleManager {
-		return sharedManager
-	}
-
-	func submit(sample: Sample, completion: (ErrorType?)->Void) {
-		queue.append((sample, completion))
-		if active == nil {
-			process(nil)
-		}
-	}
-
-	private func process(completedTask: AWSTask?) {
-		if let task = completedTask {
-			var err: SampleManagerError?
-
-			if let error = task.error {
-				NSLog("Sample Manager: \(error)")
-				err = SampleManagerError.SubmissionError(error: error)
-			}
-
-			if let exception = task.exception {
-				NSLog("Sample Manager: \(exception)")
-				err = SampleManagerError.SubmissionException(exception: exception)
-			}
-
-			active?.completion(err)
-		}
-
-		active = nil
-
-		if queue.count > 0 {
-			active = queue.removeFirst()
-
-			do {
-				try upload(active!)
-			} catch SampleManagerError.TransferUtilityUnavailable {
-				NSLog("Sample Manager: Transfer Utility is Unavailable")
-				active?.completion(SampleManagerError.TransferUtilityUnavailable)
-			} catch SampleManagerError.InvalidFileName(let url) {
-				NSLog("Sample Manager: Invalid Filename Provided")
-				active?.completion(SampleManagerError.InvalidFileName(url: url))
-			} catch {
-				NSLog("Sample Manager: Shit Hit The Fan")
-			}
-			
-		}
-	}
-
-	private func upload(submission: SampleSubmission) throws {
-		guard let transfer = AWSS3TransferUtility.defaultS3TransferUtility() else { throw SampleManagerError.TransferUtilityUnavailable }
-
-        guard let videoFile = submission.sample.media.path.flatMap({String($0.characters.dropFirst())}) else { throw SampleManagerError.InvalidFileName(url: submission.sample.media) }
-
-		let videoUrl = NSURL(fileURLWithPath: NSTemporaryDirectory() + videoFile)
-
-		let uploadExpr = AWSS3TransferUtilityUploadExpression()
-		uploadExpr.setValue("REDUCED_REDUNDANCY", forRequestParameter: "x-amz-storage-class")
-
-		transfer.uploadFile(videoUrl,
-			bucket: PartyUpConstants.StorageBucket,
-			key: videoFile,
-			contentType: videoUrl.mime,
-			expression: uploadExpr,
-			completionHander: nil).continueWithSuccessBlock({ (task) in
-				return AWSDynamoDBObjectMapper.defaultDynamoDBObjectMapper().save(submission.sample.dynamo)}).continueWithBlock({ (task) in
-					try! NSFileManager.defaultManager().removeItemAtURL(videoUrl)
-
-					dispatch_async(dispatch_get_main_queue()) { self.process(task) }
-
-					return nil
-				})
-	}
+    enum SubmissionError: ErrorType
+    {
+        case TransferUtilityUnavailable
+        case InvalidFileName(url: NSURL)
+        case SubmissionError(error: NSError)
+        case SubmissionException(exception: NSException)
+    }
+    
+    private enum SubmissionState {
+        case Idle
+        case Upload(task: AWSTask)
+        case Record(task: AWSTask)
+    }
+    
+    private var state: SubmissionState = .Idle
 }
