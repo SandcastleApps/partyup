2c7efb93fb6e0ee8f1c64645e7cbbbfdfa7413d1
Fritz Vander Heide <etherboy@me.com>
2016-03-19 11:06:57 -0300

    Prevent the constant refetching of venue samples as a citywide update occurs by using the rate limit (timeliness) capabilities of venues fetching samples and promotions.

--- PartyUP/PartyPlace.swift
+++ PartyUP/PartyPlace.swift
@@ -15,7 +15,11 @@ import Flurry_iOS_SDK
 
 class PartyPlace : FetchQueryable {
 	static let CityUpdateNotification = "CityUpdateNotification"
-    static let CityUpdateThrottlingInterval = 86400.0
+
+	private struct ThrottlingIntervalConstants {
+		static let CityUpdate = 86400.0
+		static let SampleUpdate = 30.00
+	}
 
 	let place: LMAddress
 	let pregame: Venue
@@ -39,12 +43,14 @@ class PartyPlace : FetchQueryable {
 		let unique = String(format: "*%@$%@$%@*", place.locality, place.administrativeArea, place.country)
 		let name = place.locality + " " + NSLocalizedString("Pregame Feed", comment: "Place name suffix for pregame venue")
 		self.pregame = Venue(unique: unique, open: 0, close: 0, name: name, details: nil, vicinity: place.administrativeArea, location: CLLocation(latitude: place.coordinate.latitude, longitude: place.coordinate.longitude))
+		self.pregame.fetchSamples()
+		self.pregame.fetchPromotion()
 
 		Advertisement.fetch(place)
 	}
 
 	func fetch(radius: Int, categories: String) {
-		if abs(lastFetchStatus.completed.timeIntervalSinceNow) > PartyPlace.CityUpdateThrottlingInterval || lastFetchStatus.error != nil  {
+		if abs(lastFetchStatus.completed.timeIntervalSinceNow) > ThrottlingIntervalConstants.CityUpdate || lastFetchStatus.error != nil  {
 			if !isFetching {
 				isFetching = true
 				let params = ["location" : "\(place.coordinate.latitude),\(place.coordinate.longitude)",
@@ -60,8 +66,11 @@ class PartyPlace : FetchQueryable {
 		} else {
 			let stale = NSUserDefaults.standardUserDefaults().doubleForKey(PartyUpPreferences.StaleSampleInterval)
 			let suppress = NSUserDefaults.standardUserDefaults().integerForKey(PartyUpPreferences.SampleSuppressionThreshold)
-			venues.forEach { $0.fetchSamples(withStaleInterval: stale, andSuppression: suppress); $0.fetchPromotion() }
-			pregame.fetchSamples(withStaleInterval: stale, andSuppression: suppress)
+			venues.forEach {
+				$0.fetchSamples(withStaleInterval: stale, andSuppression: suppress, andTimeliness: ThrottlingIntervalConstants.SampleUpdate)
+				$0.fetchPromotion(withTimeliness: ThrottlingIntervalConstants.SampleUpdate)
+			}
+			pregame.fetchSamples(withStaleInterval: stale, andSuppression: suppress, andTimeliness: ThrottlingIntervalConstants.SampleUpdate)
             NSNotificationCenter.defaultCenter().postNotificationName(PartyPlace.CityUpdateNotification, object: self)
 		}
 	}
@@ -71,7 +80,15 @@ class PartyPlace : FetchQueryable {
 			let json = JSON(data: response.data!)
             let local = json["results"].arrayValue.map{Venue(venue: $0)}
             
-            dispatch_async(dispatch_get_main_queue()) { self.venues.unionInPlace(local) }
+            dispatch_async(dispatch_get_main_queue()) {
+				self.venues.unionInPlace(local)
+				let stale = NSUserDefaults.standardUserDefaults().doubleForKey(PartyUpPreferences.StaleSampleInterval)
+				let suppress = NSUserDefaults.standardUserDefaults().integerForKey(PartyUpPreferences.SampleSuppressionThreshold)
+				self.venues.forEach {
+					$0.fetchSamples(withStaleInterval: stale, andSuppression: suppress, andTimeliness: ThrottlingIntervalConstants.SampleUpdate)
+					$0.fetchPromotion(withTimeliness: ThrottlingIntervalConstants.SampleUpdate)
+				}
+			}
 
 			if let next = json["next_page_token"].string {
 				dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * Int64(NSEC_PER_SEC)), dispatch_get_main_queue()) {
