1974ee66158271389c1df1e55a2f880e927f7732
Fritz Vander Heide <etherboy@me.com>
2016-04-16 13:48:21 -0300

    Implement Facebook button delegate methods in the login controller.

--- PartyUP/LoginController.swift
+++ PartyUP/LoginController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import AWSCore
 
-class LoginController: UIViewController {
+class LoginController: UIViewController, FBSDKLoginButtonDelegate {
 
+	@IBOutlet weak var facebookButton: FBSDKLoginButton!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        facebookButton.delegate = self
+		facebookButton.readPermissions = ["public_profile", "email"]
     }
+
+	// MARK: - Facebook Login
+
+	func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+		return true
+	}
+
+	func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+		if let error = error {
+			let alert = UIAlertController(
+				title: NSLocalizedString("Facebook Login Failed", comment: "Facebook login error title"),
+				message: error.localizedDescription, preferredStyle: .Alert)
+			presentViewController(alert, animated: true, completion: nil)
+		} else if result.isCancelled {
+			//handle cancelled
+		} else {
+			if let creds = AWSServiceManager.defaultServiceManager().defaultServiceConfiguration.credentialsProvider as? AWSCognitoCredentialsProvider {
+				if result.token != nil {
+					creds.logins[AWSCognitoLoginProviderKey.Facebook.rawValue] = result.token
+				}
+			}
+		}
+	}
+
+	func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+		
+	}
 }
