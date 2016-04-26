3e49068abd2f9b503e745c18b807af373ec7fea0
Fritz Vander Heide <etherboy@me.com>
2016-04-26 12:50:52 -0300

    Hide the close button if it's label is passed in as nil.

--- PartyUP/HudUtilities.swift
+++ PartyUP/HudUtilities.swift
@@ -12,23 +12,28 @@ typealias AlertHandler = DismissBlock
 
 func alertSuccessWithTitle(title: String, andDetail detail: String = String(), closeLabel close: String? = nil, dismissHandler dismiss: AlertHandler? = nil) {
 	let duration = close == nil ? 2.5 : 0.0
-	let alert = SCLAlertView().showSuccess(title, subTitle: detail, closeButtonTitle: close, duration: duration)
+	let alert = SCLAlertView()
+	alert.showCloseButton = close != nil
+	let responder = alert.showSuccess(title, subTitle: detail, closeButtonTitle: close, duration: duration)
 	if let dismiss = dismiss {
-		alert.setDismissBlock(dismiss)
+		responder.setDismissBlock(dismiss)
 	}
 }
 
 func alertFailureWithTitle(title: String, andDetail detail: String = String(), closeLabel close: String? = nil, dismissHandler dismiss: AlertHandler? = nil) {
 	let duration = close == nil ? 4.0 : 0.0
-	let alert = SCLAlertView().showError(title, subTitle: detail, closeButtonTitle: close, duration: duration)
+	let alert = SCLAlertView()
+	alert.showCloseButton = close != nil
+	let responder = alert.showError(title, subTitle: detail, closeButtonTitle: close, duration: duration)
 	if let dismiss = dismiss {
-		alert.setDismissBlock(dismiss)
+		responder.setDismissBlock(dismiss)
 	}
 }
 
-func alertWaitWithTitle(title: String = NSLocalizedString("Working Hard", comment: "Please wait"), andDetail detail: String = String(), dismissHandler dismiss: AlertHandler? = nil) -> SCLAlertViewResponder {
+func alertWaitWithTitle(title: String = NSLocalizedString("Working Hard", comment: "Please wait"), andDetail detail: String = String(), closeButton close: String? = NSLocalizedString("Cancel", comment: "Cancel button label"), dismissHandler dismiss: AlertHandler? = nil) -> SCLAlertViewResponder {
 	let alert = SCLAlertView()
-	let wait = alert.showWait(title, subTitle: detail, closeButtonTitle: NSLocalizedString("Cancel", comment: "Cancel button label"), colorStyle: 0xF45E63)
+	alert.showCloseButton = close != nil
+	let wait = alert.showWait(title, subTitle: detail, closeButtonTitle: close, colorStyle: 0xF45E63)
 	if let dismiss = dismiss {
 		wait.setDismissBlock(dismiss)
 	}
