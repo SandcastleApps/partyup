8ed840cd16b98cf8cdf5989a956cdc844d63976b
Fritz Vander Heide <etherboy@me.com>
2016-05-02 10:18:22 -0300

    Implement utility function for adding effects to a video via layers.  Will be used to put a watermark on PartyUP videos.

---
+++ PartyUP/VideoUtilities.swift
@@ -0,0 +1,53 @@
+//
+//  VideoUtilities.swift
+//  PartyUP
+//
+//  Created by Fritz Vander Heide on 2016-05-01.
+//  Copyright © 2016 Sandcastle Application Development. All rights reserved.
+//
+
+import AVFoundation
+
+typealias VideoEffectApplicator = (AVMutableVideoComposition, CGSize) -> Void
+typealias VideoExportCompletion = (AVAssetExportSession) -> Void
+
+func applyToVideo(fromInput input: NSURL, toOutput output: NSURL, effectApplicator effect: VideoEffectApplicator, exportCompletionHander exportHandler: VideoExportCompletion) {
+	let asset = AVAsset(URL: input)
+	let composition = AVMutableComposition()
+	let video = composition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+	let audio = composition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+
+	do {
+		if let videoAssetTrack = asset.tracksWithMediaType(AVMediaTypeVideo).first, audioAssetTrack = asset.tracksWithMediaType(AVMediaTypeAudio).first
+		{
+			try video.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), ofTrack: videoAssetTrack, atTime: kCMTimeZero)
+			try audio.insertTimeRange(CMTimeRangeMake(kCMTimeZero, asset.duration), ofTrack: audioAssetTrack, atTime: kCMTimeZero)
+
+			let videoInstruction = AVMutableVideoCompositionInstruction()
+			videoInstruction.timeRange = CMTimeRangeMake(kCMTimeZero, asset.duration)
+
+			let videoComposition = AVMutableVideoComposition()
+
+			videoComposition.renderSize = videoAssetTrack.naturalSize
+			videoComposition.instructions = [videoInstruction]
+			videoComposition.frameDuration = CMTimeMake(1, 30)
+
+			effect(videoComposition, videoAssetTrack.naturalSize)
+
+			if let exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality) {
+				exporter.outputURL = output
+				exporter.outputFileType = AVFileTypeQuickTimeMovie
+				exporter.shouldOptimizeForNetworkUse = true
+				exporter.videoComposition = videoComposition
+
+				exporter.exportAsynchronouslyWithCompletionHandler({
+					dispatch_async(dispatch_get_main_queue(),{
+						exportHandler(exporter)
+					})
+				})
+			}
+		}
+	} catch {
+
+	}
+}
