cda8140a242c9815afe9eafa95b43d6b337e6255
Fritz Vander Heide <etherboy@me.com>
2016-05-06 13:03:31 -0300

    Implement a wrapper and converter for addresses because such information is coming from multiple places (location picker view and LMGeocoder) and need to be normalized for use through the application.

---
+++ PartyUP/Address.swift
@@ -0,0 +1,52 @@
+//
+//  Address.swift
+//  PartyUP
+//
+//  Created by Fritz Vander Heide on 2016-05-06.
+//  Copyright © 2016 Sandcastle Application Development. All rights reserved.
+//
+
+import CoreLocation
+import LMGeocoder
+
+struct Address {
+	let coordinate: CLLocationCoordinate2D
+	let city: String
+	let province: String
+	let country: String
+
+	var location: CLLocation {
+		return CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+	}
+
+	init(coordinate: CLLocationCoordinate2D, address: [String:String]) {
+		self.coordinate = coordinate
+		self.city = address["city"] ?? "Anoncity"
+		self.province = address["province"] ?? "Anonstate"
+		self.country = address["country"] ?? "Anoncountry"
+	}
+
+	init(coordinate: CLLocationCoordinate2D, address: LMAddress) {
+		self.coordinate = coordinate
+		self.city = address.locality
+		self.province = address.administrativeArea
+		self.country = address.country
+	}
+
+	init(coordinate: CLLocationCoordinate2D, mapkitAddress address: [NSObject:AnyObject]) {
+		self.coordinate = coordinate
+		self.city = address["City"] as? String ?? "Anoncity"
+		self.province = address["State"] as? String ?? "Anonstate"
+		self.country = address["Country"] as? String ?? "Anoncountry"
+	}
+
+	static func addressForCoordinates(coordinate: CLLocationCoordinate2D, completionHandler: (Address?, NSError?) -> Void) {
+		LMGeocoder().reverseGeocodeCoordinate(coordinate, service: .AppleService) { (places, error) in
+			var address: Address?
+			if let place = places.first as? LMAddress {
+				address = Address(coordinate: coordinate, address: place)
+			}
+			completionHandler(address, error)
+		}
+	}
+}
