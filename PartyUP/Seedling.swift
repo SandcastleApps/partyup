3afffe0220b31ecb87c75c848f9809701b85cfda
Fritz Vander Heide <etherboy@me.com>
2016-07-19 12:33:25 -0300

    Switch inner lookup to use of Alamofire and SwiftyJSON.

--- PartyUP/Seedling.swift
+++ PartyUP/Seedling.swift
@@ -14,44 +14,45 @@ import SwiftyJSON
 import Alamofire
 
 extension Venue {
-    func fetchSeedlings() {
-        if let token = FBSDKAccessToken.currentAccessToken() {
-            Alamofire.request(.GET,
-                "https://graph.facebook.com/v2.7/search", parameters: ["q":name,"type":"place","center":"\(location.coordinate.latitude),\(location.coordinate.longitude)","distance":"\(100)","fields":"link","access_token":token.tokenString]).responseJSON { response in
-                    switch response.result {
-                    case .Success(let data):
-                        let places = JSON(data)
-                        if let id = places["data"][0]["id"].string {
-                            FBSDKGraphRequest(graphPath: "/\(id)", parameters: ["fields":"video_broadcasts{video{source,description,from,updated_time}}"]).startWithCompletionHandler {
-                                (connection, page, error) in
-                                var seeders = [Seedling]()
-                                if let broadcasts = page["video_broadcasts"] as? [String:AnyObject], let videos = broadcasts["data"] as? [AnyObject] {
-                                    for video in videos {
-                                        if let item = video["video"] as? [String:AnyObject]{
-                                            guard let source = (item["source"] as? String).flatMap({NSURL(string: $0)}),
-                                                let time = (item["updated_time"] as? String).flatMap({$0.toDate(DateFormat.ISO8601)})  else { continue }
-                                            let comment = item["description"] as? String
-                                            let alias = (item["from"] as? [String:AnyObject])?["name"] as? String
-                                            seeders.append(Seedling(user: NSUUID(), alias: alias, event: self, time: time, comment: comment, media: source, via: "Facebook"))
-                                        }
-                                    }
-                                }
-                            
-                                dispatch_async(dispatch_get_main_queue()) {
-                                    self.seeds = seeders
-                                }
-                            }
-                        }
-                    case .Failure(let error):
-                        print("Error fetching places: \(error)")
-                    }
-                    
-                    
-            }
+	func fetchSeedlings() {
+		if let token = FBSDKAccessToken.currentAccessToken() {
+			Alamofire.request(.GET,
+				"https://graph.facebook.com/v2.7/search", parameters: ["q":name,"type":"place","center":"\(location.coordinate.latitude),\(location.coordinate.longitude)","distance":"\(100)","fields":"link","access_token":token.tokenString]).responseJSON { response in
+					switch response.result {
+					case .Success(let data):
+						let places = JSON(data)
+						if let id = places["data"][0]["id"].string {
+							Alamofire.request(.GET,
+								"https://graph.facebook.com/v2.7/\(id)", parameters: ["fields":"video_broadcasts{video{source,description,from,updated_time}}","access_token":token.tokenString]).responseJSON(queue: dispatch_get_main_queue()) { response in
+									var seeders = [Seedling]()
+									switch response.result {
+									case .Success(let data):
+										let page = JSON(data)
+										for cast in page["video_broadcasts"]["data"].arrayValue {
+											let video = cast["video"]
+												guard let source = video["source"].string.flatMap({ NSURL(string: $0) }),
+													let time = video["updated_time"].string.flatMap({ $0.toDate(DateFormat.ISO8601) }) else { continue }
+												let comment = video["description"].string
+												let alias = video["from"]["name"].string
+												seeders.append(Seedling(user: NSUUID(), alias: alias, event: self, time: time, comment: comment, media: source, via: "Facebook"))
+											}
+
+									case .Failure(let error):
+										print("Error fetching movies: \(error)")
+									}
+
+									self.seeds = seeders
+							}
+						}
+					case .Failure(let error):
+						print("Error fetching places: \(error)")
+						self.seeds = [Seedling]()
+					}
+			}
 		} else {
 			self.seeds = [Seedling]()
 		}
-    }
+	}
 }
 
 class Seedling: Tastable {
