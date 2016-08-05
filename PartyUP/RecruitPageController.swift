a0273b41105d604737533147dfc6f164dc1872d1
Fritz Vander Heide <etherboy@me.com>
2016-08-05 15:20:26 -0300

    Tweak the label asking a user to login depending on whether the user is logged in or not.

--- PartyUP/RecruitPageController.swift
+++ PartyUP/RecruitPageController.swift
@@ -11,8 +11,23 @@ import UIKit
 class RecruitPageController: UIViewController, PageProtocol {
 
 	var page: Int!
-
 	var ad: NSURL?
+    
+    @IBOutlet weak var loginLabel: UILabel! {
+        didSet {
+            updateLoginPrompt()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(RecruitPageController.updateLoginPrompt), name: AuthenticationManager.AuthenticationStatusChangeNotification, object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
 
 	override func viewDidAppear(animated: Bool) {
 		super.viewDidAppear(animated)
@@ -25,4 +40,12 @@ class RecruitPageController: UIViewController, PageProtocol {
 			avc.url = ad
 		}
 	}
+    
+    @objc private func updateLoginPrompt() {
+        if !AuthenticationManager.shared.isLoggedIn {
+            loginLabel.text = NSLocalizedString("Login to see Facebook posts from venues!", comment: "Login prompt - not logged in")
+        } else {
+            loginLabel.text = NSLocalizedString("Post a video or share PartyUP with friends!", comment: "Login prompt - logged in")
+        }
+    }
 }
